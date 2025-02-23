1c92add4fbdc52ec77d12c0e6108b1982b5c2c26
Zizel <danbua999@gmail.com>
2025-02-23 18:18:01 +0700

    upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "2.0.0"
+DESCRIPTION = "A collection of Python function that I used in my projects."
+with open("README.md") as file:
+    LONG_DESCRIPTION = file.read()
+
+setup(
+    name = "ziz_utils",
+    version = VERSION,
+    author = "Zizel",
+    author_email = "danbua999@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/uwungu01-rep/ziz_utils",
+    packages = find_packages(),
+    install_requires = [], 
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
